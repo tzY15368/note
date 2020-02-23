8fa17fb3ccba8bd970ad37bef405c8f4ff8ef528
Nathan <2889205153@qq.com>
2020-02-23 14:21:02 +0800

    logout&psw change

--- src/containers/main/mid-col/main-disp/main-page/setting/setting-pages/security/security-logout.jsx
+++ src/containers/main/mid-col/main-disp/main-page/setting/setting-pages/security/security-logout.jsx
@@ -1,9 +1,32 @@
 import React,{Component} from 'react'
+import { Modal, Button } from 'antd';
+import {Redirect} from "react-router";
+import cookie from 'react-cookies'
+const { confirm } = Modal;
 export default class LogOut extends Component {
+    state = {
+        redirect:false
+    }
+    showConfirm = ()=> {
+        let that = this;
+        confirm({
+            title: '是否注销？',
+            onOk() {
+                that.setState({redirect:true});
+                setTimeout(cookie.remove('token'), 300);
+            },
+            onCancel() {},
+        });
+    }
     render(){
+        if(this.state.redirect===true){
+            return (
+                    <Redirect to={'/user'}/>
+                )
+        }
         return (
             <div>
-                this is logout
+                <Button block type={'danger'} size={"large"} icon={'logout'} onClick={this.showConfirm}>注销</Button>
             </div>
         )
     }
--- src/containers/main/mid-col/main-disp/main-page/setting/setting-pages/security/security-psw-change.jsx
+++ src/containers/main/mid-col/main-disp/main-page/setting/setting-pages/security/security-psw-change.jsx
@@ -1,16 +1,127 @@
 import React,{Component} from 'react'
-import { Steps, Icon } from 'antd';
-
+import {Steps, Icon, Button, Result, Input} from 'antd';
+import './security.css'
+import {Redirect} from "react-router";
+import axios from 'axios'
+import BASEURL from "../../../../../../../../configs/names";
+import cookie from 'react-cookies'
+import {ErrorMsg} from "../../../../../../../../components/notifications";
 const { Step } = Steps;
 export default class PasswordChange extends Component {
+    state = {
+        current:0,
+        pswInput:'',
+        promptMsg:'输入旧密码:',
+        loading:false,
+        redirect:false
+    };
+    next() {
+        const current = this.state.current + 1;
+        this.setState({ current });
+    }
+
+    prev() {
+        const current = this.state.current - 1;
+        this.setState({ current });
+    }
+    handleChange = (e)=>{
+        console.log(e.target.value);
+        this.setState({pswInput:e.target.value});
+    };
+    handleSubmit = (e)=>{
+        this.setState({ loading:true });
+        const url = BASEURL+'/user';
+        const token = cookie.load('token');
+        if(this.state.current===0){
+            if(token!==undefined){
+                axios.put(url,{operationType:1,password:this.state.pswInput},{headers:{Authorization:token}
+                }).then((res)=>{
+                    if(res.data.success===1){
+                        this.setState({current:this.state.current+1,loading:false,pswInput:'',promptMsg:'输入新密码'})
+                    }
+                    else {
+                        this.setState({loading:false,pswInput:'',promptMsg:res.data.data})
+                    }
+                }).catch((err)=>{ErrorMsg('网络错误：'+err)});
+            } else {ErrorMsg('cookie error')}
+        } else if(this.state.current===1){
+            if(token!==undefined){
+                axios.put(url,{operationType:2,password:this.state.pswInput},{headers:{Authorization:token}
+                }).then((res)=>{
+                    if(res.data.success===1){
+                        this.setState({current:this.state.current+1,loading:false,pswInput:'',promptMsg:'not sure'});
+                        cookie.remove('token');
+                        setTimeout(()=>{this.setState({redirect:true})},2500)
+                    }
+                    else {
+                        this.setState({loading:false,pswInput:'',promptMsg:res.data.data})
+                    }
+                }).catch((err)=>{ErrorMsg('网络错误：'+err)});
+            } else {ErrorMsg('cookie error')}
+        }
+    };
+    handleRedirect = ()=>{
+        this.setState({redirect:true})
+    }
     render(){
+        const {current,promptMsg,loading} = this.state;
+        const inputs = (
+            <div>
+                <div className="form-group">
+                    <span><h5>{promptMsg}</h5></span>
+                </div>
+                <div className="form-group" style={{marginTop:10}}>
+                    <Input.Password
+                        prefix={<Icon type="lock"/>}
+                        name="oldPsw"
+                        placeholder="Enter password"
+                        onChange={this.handleChange}
+                        allowClear
+                        value={this.state.pswInput}
+                    />
+                </div>
+                <div className="form-group" style={{textAlign:'right'}}>
+                    <Button type="primary" onClick={this.handleSubmit} loading={loading}>下一步</Button>
+                </div>
+            </div>
+        );
+        const steps = [
+            {
+                title: '验证',
+                content: inputs
+            },
+            {
+                title: '更新',
+                content: inputs,
+            },
+            {
+                title: '完成',
+                content: (
+                    <Result
+                        status='success'
+                        title="更改成功"
+                        subTitle="三秒后重定向至登陆页"
+                        extra={[
+                            <Button key="buy" onClick={this.handleRedirect}>手动跳转</Button>,
+                        ]}
+                    />
+                ),
+            },
+        ];
+        if(this.state.redirect===true){
+            return(
+                <Redirect to={'/user'}/>
+            )
+        }
         return (
-            <Steps current={2}>
-                <Step status="finish" title="Login" icon={<Icon type="user" />} />
-                <Step status="finish" title="Verification" icon={<Icon type="solution" />} />
-                <Step status="process" title="Pay" icon={<Icon type="loading" />} />
-                <Step status="wait" title="Done" icon={<Icon type="smile-o" />} />
-            </Steps>
+            <div>
+                <Steps current={current}>
+                    {steps.map(item => (
+                        <Step key={item.title} title={item.title} />
+                    ))}
+                </Steps>
+                <div className="steps-content">{steps[current].content}</div>
+            </div>
         )
     }
 }
